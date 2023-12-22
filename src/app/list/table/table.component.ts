95c4e2bcb0411a59c43077553caf8e818e42dc65
blue-acer <110229245+blue-acer@users.noreply.github.com>
2023-12-22 03:54:09 +0000

    update

--- src/app/list/table/table.component.ts
+++ src/app/list/table/table.component.ts
@@ -3,6 +3,7 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import { CharacterDataService } from '../../services/character-data.service';
+import { ICharacter } from '../../models/ICharacter';
 
 @Component({
   selector: 'app-table',
@@ -10,17 +11,17 @@ import { CharacterDataService } from '../../services/character-data.service';
   styleUrl: './table.component.scss'
 })
 export class TableComponent implements AfterViewInit, OnInit{
-  displayedColumns: string[] = ['name'];
-  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['name','gender','birth_year'];
+  dataSource!: MatTableDataSource<ICharacter>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-
+  
   constructor(private characterDataService:CharacterDataService) { }
 
   ngOnInit(): void {
     this.getCharacterList();
-  }
+      }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -35,7 +36,7 @@ export class TableComponent implements AfterViewInit, OnInit{
       this.dataSource.paginator.firstPage();
     }
   }
-  getCharacterList(){
+    getCharacterList(){
     return this.characterDataService.getList()
     .subscribe({
       next:(res)=>{
